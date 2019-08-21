7dc78382e5a80b6021b11363428dc6c3410ca6ac
3jayjoun <pro.omarjribi@gmail.com>
2019-08-21 01:09:58 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Photo from './Photo.js'
+import Title from './Title.js';
+import Wrapper from './Wrapper';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <Wrapper>
+
+      <Photo src="/profile.jpg" />
+
+      <Title style={{color:"red"}}>My Name here</Title>
+
+      <Title small>My job here</Title>
+      
+      </Wrapper>
+    
   );
 }
 
